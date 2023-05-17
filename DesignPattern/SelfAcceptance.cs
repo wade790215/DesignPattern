638b155e4877adf855f518536c3255425c0272ef
MpWade <wade.liu@varlivetech.com>
2023-05-17 20:31:57 +0800

    [更新] 自我測驗更新(觀察者未實作)

--- DesignPattern/SelfAcceptance.cs
+++ DesignPattern/SelfAcceptance.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Policy;
 
 namespace DesignPattern
 {
     internal class SelfAcceptance
     {
-
         internal void Main()
         {
             CharacterDirector characterDirector = new CharacterDirector();
@@ -13,7 +13,7 @@ namespace DesignPattern
             CharacterBuilder enemyBuilder = new CharacterBuilder();
             CharacterParameter playerParameter = new CharacterParameter() { Health = 100, Attack = 15, Defense = 5, Speed = 3 };
             CharacterParameter enemyParameter = new CharacterParameter() { Health = 10, Attack = 2, Defense = 1, Speed = 1 };
-            
+
 
             Character player = characterDirector.CreateCharacter(playerBuilder, playerParameter);
             Character enemy = characterDirector.CreateCharacter(enemyBuilder, enemyParameter);
@@ -21,24 +21,23 @@ namespace DesignPattern
             ICharacterSkill fireBall = new FireBall();
             ICharacterSkill clash = new Clash();
 
-            playerBuilder.SetSkill(clash);
-            enemyBuilder.SetSkill(fireBall);
+            playerBuilder.InitSkill(clash);
 
-            player.Skill.UseSkill();
-            enemy.Skill.UseSkill();
+            player.UseSkill(fireBall);
+            player.UseSkill(clash);
 
-            var helmet = new Helmet(player);
-            var shoes = new Shoes(player);
-            helmet.Equip();
-            shoes.Equip();
+            player.LearnSkill(fireBall);
+            player.UseSkill(fireBall);
+
+            player.Equip<Helmet>();
+            player.Equip<Shoes>();
+
             Console.WriteLine(player.Defense);
             Console.WriteLine(player.Speed);
 
-            helmet.UnEquip();
+            player.Unequip<Helmet>();
             Console.WriteLine(player.Defense);
         }
-
-       
     }
 
     public class CharacterDirector
@@ -62,13 +61,97 @@ namespace DesignPattern
         public int Speed { get; set; }
     }
 
-    public class Character
+    public class Character:IUpdateCharacterProp
     {
+        public Character()
+        {
+            characterSkills = new List<ICharacterSkill>();
+            equipments = new List<IEquipable>();
+        }
+
         public int Health { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
         public int Speed { get; set; }
-        public ICharacterSkill Skill { get; set; }
+
+        private List<ICharacterSkill> characterSkills;
+
+        private List<IEquipable> equipments;
+
+        //泛型可以直接使用類別進行裝備
+        public void Equip<T>() where T : IEquipable, new()
+        {
+            var item = new T();
+            if (!equipments.Contains(item))
+            {
+                item.Equip(this);
+                equipments.Add(item);
+            }
+        }
+
+        public void Unequip<T>() where T : IEquipable
+        {
+            var item = equipments.Find(e => e.GetType() == typeof(T));
+            if (item != null)
+            {
+                item.UnEquip(this);
+                equipments.Remove(item);
+            }
+        }
+
+        //使用時還需要實例化相對的技能物件
+        public void LearnSkill(ICharacterSkill characterSkill)
+        {
+            if (!characterSkills.Contains(characterSkill))
+                characterSkills.Add(characterSkill);
+        }
+
+        public void ForgetSkill(ICharacterSkill characterSkill)
+        {
+            var skill = characterSkills.Find(e => e == characterSkill);
+            if (skill != null)
+            {
+                characterSkills.Remove(skill);
+            }
+        }
+
+        public void UseSkill(ICharacterSkill characterSkill)
+        {
+            var skill = characterSkills.Find(e => e == characterSkill);
+            if (skill != null)
+            {
+                skill.UseSkill();
+            }
+            else
+            {
+                Console.WriteLine($"還沒學會這個技能");
+            }
+        }
+
+        public void IncreaseDefense(int value)
+        {
+            Defense += value;
+        }
+
+        public void DecreaseDefense(int value)
+        {
+            Defense -= value;
+        }
+
+        public void IncreaseSpeed(int value)
+        {
+            Speed += value;
+        }
+
+        public void DecreaseSpeed(int value)
+        {
+            Speed -= value;
+        }
+
+        public void UpdateProp(Character character)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class CharacterBuilder
@@ -85,9 +168,9 @@ namespace DesignPattern
             return character;
         }
 
-        public void SetSkill(ICharacterSkill skill)
+        public void InitSkill(ICharacterSkill skill)
         {
-            GetCharacter().Skill = skill;
+            GetCharacter().LearnSkill(skill);
         }
 
         public void SetHealth(int health)
@@ -132,53 +215,40 @@ namespace DesignPattern
         }
     }
 
-    public class CharacterDecorator
-    {
-        protected Character character;
-
-        public CharacterDecorator(Character character)
-        {
-            this.character = character;
-        }
-    }
-
     public interface IEquipable
     {
-        void Equip();
-        void UnEquip();
+        void Equip(Character character);
+        void UnEquip(Character character);
     }
 
-    public class Helmet : CharacterDecorator, IEquipable
+    public class Helmet : IEquipable
     {
-        public Helmet(Character character) : base(character)
+        public void Equip(Character character)
         {
+            character.IncreaseDefense(10);
         }
 
-        public void Equip()
+        public void UnEquip(Character character)
         {
-            character.Defense += 10;
-        }
-
-        public void UnEquip()
-        {
-            character.Defense -= 10;
+            character.DecreaseDefense(10);
         }
     }
 
-    public class Shoes : CharacterDecorator, IEquipable
+    public class Shoes : IEquipable
     {
-        public Shoes(Character character) : base(character)
+        public void Equip(Character character)
         {
+            character.IncreaseSpeed(3);
         }
 
-        public void Equip()
+        public void UnEquip(Character character)
         {
-            character.Speed += 5;
+            character.DecreaseSpeed(3);
         }
+    }
 
-        public void UnEquip()
-        {
-            character.Speed -= 5;
-        }
+    public interface IUpdateCharacterProp
+    {
+        void UpdateProp(Character character);
     }
 }
