1ba63499e190989fc83a9a8290a03afcc785eb93
Bachider <w0652039@gmail.com>
2024-01-19 08:33:03 +0800

    中介者練習到一半

--- DesignPattern/MediatorPattern/MediatorPattern.cs
+++ DesignPattern/MediatorPattern/MediatorPattern.cs
@@ -11,17 +11,75 @@ namespace DesignPattern
     {
         internal void Main()
         {
-            Mediator mediator = new PM();
-            Colleague programmer = new Programmer(mediator, "前端");
-            Colleague art = new Art(mediator, "美術");
-            mediator.AddColleague(programmer);
-            mediator.AddColleague(art);
+            //Mediator mediator = new PM();
+            //Colleague programmer = new Programmer(mediator, "前端");
+            //Colleague art = new Art(mediator, "美術");
+            //mediator.AddColleague(programmer);
+            //mediator.AddColleague(art);
 
-            programmer.SendTo("幫我放物件到美術場景內", art);
-            art.SendAllExceptMyself("前端們幫我開發好用工具");
+            //programmer.SendTo("幫我放物件到美術場景內", art);
+            //art.SendAllExceptMyself("前端們幫我開發好用工具");
+
+            ChatRoom chatRoom = new ChatRoom();
+
+            ChatUser user1 = new ChatUser("1", "阿德");
+            ChatUser user2 = new ChatUser("2", "阿明");
+
+            chatRoom.AddUser(user1);
+            chatRoom.AddUser(user2);
+
+            chatRoom.SendMessageTo("大家好", "1");   
         }
     }
 
+    #region Pratice 1
+
+    public interface IChatRoom
+    {
+        void SendMessageTo(string message, string userId);
+    }
+
+    public class ChatRoom : IChatRoom
+    {
+        private List<ChatUser> chatUsers = new List<ChatUser>(); 
+        
+        public void AddUser(ChatUser chatUser)
+        {
+            chatUsers.Add(chatUser);
+        }
+
+        public void RemoveUser(ChatUser chatUser)
+        {
+            chatUsers.Remove(chatUser);
+        }
+
+        public void SendMessageTo(string message, string userId)
+        {
+            if (chatUsers.Exists(c => c.UserId == userId))
+                Console.WriteLine($"使用者:{userId} 說:{message}");
+        }
+
+        public void SendMessageToAll(string message)
+        {
+            if (chatUsers.Count > 0)
+                chatUsers.ForEach(c => Console.WriteLine($"使用者:{c.UserId} 說:{message}"));
+        }
+    }
+
+    public class ChatUser
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public ChatUser(string userId, string userName)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+    }
+
+    #endregion
+
+    #region Pratice 2
     internal abstract class Mediator
     {
         protected List<Colleague> ColleagueList = new List<Colleague>();
@@ -107,4 +165,5 @@ namespace DesignPattern
             Console.WriteLine($"美術收到:{message}");
         }
     }
+    #endregion 
 }
