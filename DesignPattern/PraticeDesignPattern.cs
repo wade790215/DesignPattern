f3cd40ea88059b1a63e41cd2ef344cbe07be0561
WadeLiu <wadeliu@yile.com.tw>
2024-01-30 09:21:07 +0800

    練習策略模式、工廠模式

--- DesignPattern/PraticeDesignPattern.cs
+++ DesignPattern/PraticeDesignPattern.cs
@@ -16,17 +16,20 @@ namespace DesignPattern
             public void Main()
             {
                 NotiflySystem notiflySystem = new NotiflySystem();
-                MyPhone myPhone = new MyPhone();
-                notiflySystem.AddOberserve(myPhone);
-
                 HomeDeviceCommand homeDeviceCommand = new HomeDeviceCommand();
                 homeDeviceCommand.SetNotiflySystem(notiflySystem);
-                Light light = new Light();
-                Audio audio = new Audio();
-                IDeviceCommand lightOn = new LightOnCommand(light);
-                IDeviceCommand lightOff = new LightOffCommand(light);
-                IDeviceCommand audioOn = new AudioOnCommand(audio);
-                IDeviceCommand audioOff = new AudioOffCommand(audio);
+                MyPhone myPhone = new MyPhone(homeDeviceCommand);
+                notiflySystem.AddOberserve(myPhone);
+
+                DeviceFactory deviceFactory = new DeviceFactory();
+                IDeviceControl smartLight = deviceFactory.CreateDevice(new DeviceStatus("燈", "開燈", "智慧燈"));
+                IDeviceControl rayLight = deviceFactory.CreateDevice(new DeviceStatus("燈", "開燈", "雷射燈"));
+                IDeviceControl homeAudio = deviceFactory.CreateDevice(new DeviceStatus("音響", "開啟音響", "家庭劇院"));
+                IDeviceControl audio =  deviceFactory.CreateDevice(new DeviceStatus("音響", "開啟音響", "一般音響"));
+                IDeviceCommand lightOn = new LightOnCommand(smartLight);
+                IDeviceCommand lightOff = new LightOffCommand(smartLight);
+                IDeviceCommand audioOn = new AudioOnCommand(homeAudio);
+                IDeviceCommand audioOff = new AudioOffCommand(homeAudio);
                 homeDeviceCommand.AddCommand(lightOn);
                 homeDeviceCommand.AddCommand(lightOff);
                 homeDeviceCommand.AddCommand(audioOn);
@@ -34,16 +37,12 @@ namespace DesignPattern
 
                 homeDeviceCommand.Execute(lightOn);
                 homeDeviceCommand.Execute(audioOn);
+                myPhone.UseHomeStrategy(new LeavingHome());
                 Console.ReadLine();
             }
         }
 
         #region 命令模式
-        public interface IDeviceCommand
-        {
-            void Execute();
-            DeviceStatus GetDeviceStatus();
-        }
 
         public class HomeDeviceCommand
         {
@@ -80,6 +79,30 @@ namespace DesignPattern
                 }
             }
 
+            public void ExecuteAllOffCommands()
+            {
+                foreach (var deviceCommand in deviceCommands)
+                {
+                    if (deviceCommand is LightOffCommand || deviceCommand is AudioOffCommand)
+                    {
+                        deviceCommand.Execute();
+                        notiflySystem.Notify(deviceCommand.GetDeviceStatus());
+                    }
+                }
+            }
+
+            public void ExecuteAllLightOnCommands()
+            {
+                foreach (var deviceCommand in deviceCommands)
+                {
+                    if (deviceCommand is LightOnCommand)
+                    {
+                        deviceCommand.Execute();
+                        notiflySystem.Notify(deviceCommand.GetDeviceStatus());
+                    }
+                }
+            }
+
             public void Execute(IDeviceCommand deviceCommand)
             {
                 if (deviceCommands.Contains(deviceCommand))
@@ -92,9 +115,9 @@ namespace DesignPattern
 
         public class LightOnCommand : IDeviceCommand
         {
-            private Light light;
+            private IDeviceControl light;
 
-            public LightOnCommand(Light light)
+            public LightOnCommand(IDeviceControl light)
             {
                 this.light = light;
             }
@@ -112,9 +135,9 @@ namespace DesignPattern
 
         public class LightOffCommand : IDeviceCommand
         {
-            private Light light;
+            private IDeviceControl light;
 
-            public LightOffCommand(Light light)
+            public LightOffCommand(IDeviceControl light)
             {
                 this.light = light;
             }
@@ -132,9 +155,9 @@ namespace DesignPattern
 
         public class AudioOnCommand : IDeviceCommand
         {
-            private Audio audio;
+            private IDeviceControl audio;
 
-            public AudioOnCommand(Audio audio)
+            public AudioOnCommand(IDeviceControl audio)
             {
                 this.audio = audio;
             }
@@ -152,9 +175,9 @@ namespace DesignPattern
 
         public class AudioOffCommand : IDeviceCommand
         {
-            private Audio audio;
+            private IDeviceControl audio;
 
-            public AudioOffCommand(Audio audio)
+            public AudioOffCommand(IDeviceControl audio)
             {
                 this.audio = audio;
             }
@@ -170,77 +193,28 @@ namespace DesignPattern
             }
         }
 
-        public class Audio : IDevice
-        {
-            public string Brand { get; set; }
-            private DeviceStatus deviceStatus;
-
-            public Audio()
-            {
-                deviceStatus = new DeviceStatus("音響", "關閉音響","一般音響");
-            }
-
-            public void On()
-            {
-                deviceStatus.Status = "開啟音響";
-                Console.WriteLine(deviceStatus.Status);
-            }
-
-            public void Off()
-            {
-                deviceStatus.Status = "關閉音響";
-                Console.WriteLine(deviceStatus.Status);
-            }
-
-            public DeviceStatus GetDeviceStatus()
-            {
-                return deviceStatus;
-            }
-        }
-
-        public class Light : IDevice
-        {
-            public string Brand { get; set; }
-            private DeviceStatus deviceStatus;
-
-            public Light()
-            {
-                deviceStatus = new DeviceStatus("燈", "關燈","一般燈");
-            }
-
-            public void On()
-            {
-                deviceStatus.Status = "開燈";
-                Console.WriteLine(deviceStatus.Status);
-            }
-
-            public void Off()
-            {
-                deviceStatus.Status = "關燈";
-                Console.WriteLine(deviceStatus.Status);
-            }
-
-            public DeviceStatus GetDeviceStatus()
-            {
-                return deviceStatus;
-            }
-        }
-
         #endregion
 
         #region 觀察者模式
 
         public class MyPhone : IOberserve
         {
+            private HomeDeviceCommand homeDeviceCommand;    
+            public MyPhone(HomeDeviceCommand homeDeviceCommand) 
+            {
+                this.homeDeviceCommand = homeDeviceCommand; 
+            }
+
+            public void UseHomeStrategy(ISmartHomeStrategy smartHomeStrategy)
+            {
+                if(smartHomeStrategy != null)
+                    smartHomeStrategy.Execute(homeDeviceCommand);
+            }
+
             public void Update(DeviceStatus status)
             {
-                Console.WriteLine($"[MyPhone] Received Update: Device - {status.DeviceName}, Status - {status.Status}, Time - {status.Timestamp}");
+                Console.WriteLine($"[MyPhone] Received Update: Device - {status.Brand}, Status - {status.Status}, Time - {status.Timestamp}");
             }
-        }
-
-        public interface IOberserve
-        {
-            void Update(DeviceStatus deviceStatus);
         }
 
         public class NotiflySystem
@@ -273,14 +247,30 @@ namespace DesignPattern
 
         public class DeviceFactory
         {
-            public IDevice CreateDevice(DeviceStatus deviceStatus)
+            public IDeviceControl CreateDevice(DeviceStatus deviceStatus)
             {
                 switch(deviceStatus.DeviceName)
                 {
                     case "燈":
-                        return new Light();
+                        switch(deviceStatus.Brand)
+                        {
+                            case "智慧燈":
+                                return new SmartLight(deviceStatus);
+                            case "雷射燈":
+                                return new RayLight(deviceStatus);
+                            default:
+                                return new Light(deviceStatus);
+                        }
                     case "音響":
-                        return new Audio();
+                        switch (deviceStatus.Brand)
+                        {
+                            case "智慧音響":
+                                return new SmartAudio(deviceStatus);
+                            case "家庭劇院":
+                                return new HomeAudio(deviceStatus);
+                            default:
+                                return new Audio(deviceStatus);
+                        }
                     default:
                         return null;
                 }
@@ -289,10 +279,74 @@ namespace DesignPattern
 
         #endregion
 
-        public interface IDevice
+        #region 策略模式
+
+        public class LeavingHome : ISmartHomeStrategy
         {
-            string Brand { get; set; }     
+            //關閉所有的設備
+            public void Execute(HomeDeviceCommand homeDeviceCommand)
+            {
+                Console.WriteLine("\n離家模式關閉所有設備");
+                homeDeviceCommand.ExecuteAllOffCommands();  
+            }
         }
+
+        public class ComingHome : ISmartHomeStrategy
+        {
+            //打開所有的電燈
+            public void Execute(HomeDeviceCommand homeDeviceCommand)
+            {
+                homeDeviceCommand.ExecuteAllLightOnCommands();
+                Console.WriteLine("回家模式打開所有電燈");
+            }
+        }
+
+
+        #endregion
+
+        #region 裝飾者模式
+
+        public interface IDecorator
+        {
+            void Decorate();
+        }
+
+        #endregion
+
+        #region Interface
+
+        public interface ISmartHomeStrategy
+        {
+            void Execute(HomeDeviceCommand homeDeviceCommand);
+        }
+
+        public interface IDeviceInfo
+        {
+            string Brand { get; set; }
+        }
+
+        public interface IDeviceControl
+        {
+            void On();
+            void Off();
+            DeviceStatus GetDeviceStatus();
+        }
+
+        public interface IDeviceCommand
+        {
+            void Execute();
+            DeviceStatus GetDeviceStatus();
+        }
+
+        public interface IOberserve
+        {
+            void Update(DeviceStatus deviceStatus);
+        }
+
+
+        #endregion
+
+        #region Property
 
         public class DeviceStatus
         {
@@ -301,7 +355,7 @@ namespace DesignPattern
             public string Brand { get; set; }
             public DateTime Timestamp { get; set; }
 
-            public DeviceStatus(string deviceName, string status,string brand)
+            public DeviceStatus(string deviceName, string status, string brand)
             {
                 DeviceName = deviceName;
                 Status = status;
@@ -309,5 +363,95 @@ namespace DesignPattern
                 Timestamp = DateTime.Now;
             }
         }
+
+        public class Audio : IDeviceInfo, IDeviceControl
+        {
+            public string Brand { get; set; }
+            private DeviceStatus deviceStatus;
+
+            public Audio(DeviceStatus deviceStatus)
+            {
+                this.deviceStatus = deviceStatus;
+            }
+
+            public void On()
+            {
+                deviceStatus.Status = "開啟音響";
+                Console.WriteLine(deviceStatus.Status);
+            }
+
+            public void Off()
+            {
+                deviceStatus.Status = "關閉音響";
+                Console.WriteLine(deviceStatus.Status);
+            }
+
+            public DeviceStatus GetDeviceStatus()
+            {
+                return deviceStatus;
+            }
+        }
+
+        public class Light : IDeviceInfo, IDeviceControl
+        {
+            public string Brand { get; set; }
+            private DeviceStatus deviceStatus;
+
+            public Light(DeviceStatus deviceStatus)
+            {
+                this.deviceStatus = deviceStatus;
+            }
+
+            public void On()
+            {
+                deviceStatus.Status = "開燈";
+                Console.WriteLine(deviceStatus.Status);
+            }
+
+            public void Off()
+            {
+                deviceStatus.Status = "關燈";
+                Console.WriteLine(deviceStatus.Status);
+            }
+
+            public DeviceStatus GetDeviceStatus()
+            {
+                return deviceStatus;
+            }
+        }
+
+        public class SmartLight : Light
+        {
+            public SmartLight(DeviceStatus deviceStatus) : base(deviceStatus)
+            {
+                Brand = "智慧燈";
+            }
+        }
+
+        public class RayLight : Light
+        {
+            public RayLight(DeviceStatus deviceStatus) : base(deviceStatus)
+            {
+                Brand = "雷射燈";
+            }
+        }
+
+        public class SmartAudio : Audio
+        {
+            public SmartAudio(DeviceStatus deviceStatus) : base(deviceStatus)
+            {
+                Brand = "智慧音響";
+            }
+        }
+
+        public class HomeAudio : Audio
+        {
+            public HomeAudio(DeviceStatus deviceStatus) : base(deviceStatus)
+            {
+                Brand = "家庭劇院";
+            }
+        }
+
+        #endregion
     }
 }
