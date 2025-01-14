030415223e6a13a37f9e35bc38cc28b283af0402
WadeLiu <wadeliu@yile.com.tw>
2025-01-14 09:34:38 +0800

    課本練習

--- DesignPattern/DesignPatternBookPratice/DesignPatternBookPratice.cs
+++ DesignPattern/DesignPatternBookPratice/DesignPatternBookPratice.cs
@@ -6,18 +6,63 @@ namespace DesignPattern
     {
         public void Main()
         {
-            IQuckable oldDuck = new OldDuck();
-            oldDuck.Quack();
-            
-            IQuckable youngDuck = new YoungDuck();
-            youngDuck.Quack();
+            Simulator simulator = new Simulator();
+            simulator.Simulate();
         }
-        
-        public interface IQuckable
+
+        public class Simulator
         {
-            void Quack();
+            public void Simulate()
+            {
+                IQuckable oldDuck = new QuackCounter(new OldDuck());
+                IQuckable youngDuck = new QuackCounter(new YoungDuck());
+                GooseAdapter gooseAdapter = new GooseAdapter(new Goose());
+
+                Simulate(oldDuck);
+                Simulate(youngDuck);
+                gooseAdapter.Quack();
+                
+                var quackCounter = new QuackCounter(gooseAdapter);
+                Console.WriteLine($"QuackCounter:{quackCounter.GetQuacks()}");
+            }
+
+            public void Simulate(IQuckable duck)
+            {
+                duck.Quack();
+            }
         }
-        
+
+        public class QuackCounter : IQuckable
+        {
+            private IQuckable duck;
+            //靜態函數可以記錄所有QuackCounter的數量
+            private static int _numberOfQuacks;
+            
+            public QuackCounter(IQuckable duck)
+            {
+                this.duck = duck;
+            }
+
+            public void Quack()
+            {
+                duck.Quack();
+                _numberOfQuacks++;
+            }
+            
+            public int GetQuacks()
+            {
+                return _numberOfQuacks;
+            }
+        }
+
+        public class Goose
+        {
+            public void Honk()
+            {
+                Console.WriteLine("Honk");
+            }
+        }
+
         public class OldDuck : IQuckable
         {
             public void Quack()
@@ -25,12 +70,32 @@ namespace DesignPattern
                 Console.WriteLine("Quack");
             }
         }
-        
+
         public class YoungDuck : IQuckable
         {
             public void Quack()
             {
                 Console.WriteLine("Quack Quack");
+            }
+        }
+
+        public interface IQuckable
+        {
+            void Quack();
+        }
+
+        public class GooseAdapter : IQuckable
+        {
+            private Goose goose;
+
+            public GooseAdapter(Goose goose)
+            {
+                this.goose = goose;
+            }
+
+            public void Quack()
+            {
+                goose.Honk();
             }
         }
     }
